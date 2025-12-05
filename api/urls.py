2f5f968ecf352c5b032f9b71acb780ff43c548eb
islamouahab <islamouahab14@gmail.com>
2025-12-05 21:12:13 +0100

    im done twin

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import predict_hybrid,predict_single,ranks
+from .views import predict_hybrid,predict_single,ranks,search_plants
 urlpatterns = [
   path('predict/',predict_hybrid,name='predict_hybrid'),
   path('predict-single/',predict_single,name='predict_single'),
   path('ranks/',ranks,name = "ranks"),
+  path('search-plants/', search_plants, name='search_plants'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .backend import AgroX_Intelligence
+from .services import search_plants_service
+
 
 @api_view(['POST'])
 def predict_hybrid(request):
@@ -22,4 +24,22 @@ def ranks(request):
     model =AgroX_Intelligence()
     result = model.get_top_hybrids()
     return Response(result)
+@api_view(['POST'])
+def search_plants(request):
+    """
+    Receives JSON: { "query": "mallow" }
+    Returns JSON: [ 
+        { 
+          "display_name": "shrubby Indian mallow (Abutilon)", 
+          "common_name": "shrubby Indian mallow", 
+          "genus": "Abutilon" 
+        }, ... 
+    ]
+    """
+    query = request.data.get('query', '')
     
+    # 1. Use your service to find matches
+    results = search_plants_service(query)
+    
+    # 2. Return list (DRF automatically serializes lists/dicts to JSON)
+    return Response(results)
